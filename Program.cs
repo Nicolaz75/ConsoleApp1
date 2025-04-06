7831fc45c3f4d325d6b14a6642bba6fef4f89806
Pro <Pro@LAPTOP-EHMLE21H>
2025-04-06 08:26:39 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static double calculMoyenne(int [] somme,int nbre)
+        {
+            int notes = 0;
+            for(int k = 0; k<nbre; k++)
+            {
+                notes = notes + somme[k];
+            }
+
+            double moyenne = notes / nbre;
+            return moyenne;
+        }
+        static void Main(string[] args)
+        {
+            // taille du tableau
+            Console.Write("Veuillez saisir le nombre de notes : ");
+            int nbre = int.Parse(Console.ReadLine());
+
+            // déclaration du tableau
+            int[] tableau = new int[nbre];
+
+            // saisie des notes
+            for(int k = 0; k < nbre; k++)
+            {
+                Console.Write("Veuillez saisir la note numéro " + (k + 1) + "" + ": ");
+                tableau[k] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("La moyenne est " + calculMoyenne(tableau, nbre));
+            Console.ReadLine();
+
+        }
+    }
+}
